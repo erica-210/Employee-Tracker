27d20bbe6c4224998c34e984558ece984f97bbf0
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-11-12 23:08:20 -0600

    input questions and switch statments

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require("express");
+// pulling in packages
 const mysql = require("mysql2");
+const fs = require("fs");
+const inquirer = require("inquirer");
 require("dotenv").config();
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -22,3 +17,64 @@ const db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
+// Creating an array of questions for user input
+const userInput = () => {
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "How would you like to proceed?",
+          name: "actions",
+          Choices: [
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add a department",
+            "add a role",
+            "add an employee",
+            "update an employee role",
+            "Exit",
+          ],
+        },
+      ])
+
+      // sets up reaction to userInput choice
+      .then((data) => {
+        switch (data.actions) {
+          case "view all departments":
+           viewAllDepartments();
+            break;
+          case "view all roles":
+            viewAllRoles();
+            break;
+          case "view all employees":
+            viewAllEmployees();
+            break;
+          case "add a department":
+            addADepartment();
+            break;
+          case "add a role":
+            addARole();
+            break;
+          case "add an employee":
+            addAnEmployee();
+            break;
+          case "update an employee role":
+            updateAnEmployeeRole();
+            break;
+          default:
+            db.end();
+            console.log("Goodbye!");
+            break;
+        }
+      })
+  );
+};
+
+// 
+
+// close the connection when the application exits
+process.on("exit", () => {
+  db.end();
+});
